5b48d587f86ac54c8becb614ae415398c8654b47
Julian <juliannoronha07@gmail.com>
2024-09-27 19:53:26 -0400

    Boilerplate Elimination

--- demo/src/main/java/com/demoproject/demo/dto/UserProductivityDTO.java
+++ demo/src/main/java/com/demoproject/demo/dto/UserProductivityDTO.java
@@ -1,59 +1,12 @@
 package com.demoproject.demo.dto;
 
+import lombok.Value;
+
+@Value
 public class UserProductivityDTO {
-    private String username;
-    private long totalSubmissions;
-    private long totalPouchesChecked;
-    private String avgTimeDuration;
-    private double avgPouchesPerHour;
-
-    public UserProductivityDTO(String username, long totalSubmissions, long totalPouchesChecked, 
-                               String avgTimeDuration, double avgPouchesPerHour) {
-        this.username = username;
-        this.totalSubmissions = totalSubmissions;
-        this.totalPouchesChecked = totalPouchesChecked;
-        this.avgTimeDuration = avgTimeDuration;
-        this.avgPouchesPerHour = avgPouchesPerHour;
-    }
-
-    // Getters and setters
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public long getTotalSubmissions() {
-        return totalSubmissions;
-    }
-
-    public void setTotalSubmissions(long totalSubmissions) {
-        this.totalSubmissions = totalSubmissions;
-    }
-
-    public long getTotalPouchesChecked() {
-        return totalPouchesChecked;
-    }
-
-    public void setTotalPouchesChecked(long totalPouchesChecked) {
-        this.totalPouchesChecked = totalPouchesChecked;
-    }
-
-    public String getAvgTimeDuration() {
-        return avgTimeDuration;
-    }
-
-    public void setAvgTimeDuration(String avgTimeDuration) {
-        this.avgTimeDuration = avgTimeDuration;
-    }
-
-    public double getAvgPouchesPerHour() {
-        return avgPouchesPerHour;
-    }
-
-    public void setAvgPouchesPerHour(double avgPouchesPerHour) {
-        this.avgPouchesPerHour = avgPouchesPerHour;
-    }
+    String username;
+    long totalSubmissions;
+    long totalPouchesChecked;
+    String avgTimeDuration;
+    double avgPouchesPerHour;
 }
