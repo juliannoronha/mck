5b0eafb99cb21c918b6743b689308844bcf0acc9
Julian <juliannoronha07@gmail.com>
2024-10-03 16:19:37 -0400

    Added Lombokto UserProductivityQueryDTO

--- demo/src/main/java/com/demoproject/demo/dto/UserProductivityQueryDTO.java
+++ demo/src/main/java/com/demoproject/demo/dto/UserProductivityQueryDTO.java
@@ -1,43 +1,20 @@
 package com.demoproject.demo.dto;
 
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 /**
  * Data Transfer Object (DTO) for querying user productivity metrics.
  * This class encapsulates key productivity indicators for a user.
  */
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserProductivityQueryDTO {
     private String username;
     private Long totalSubmissions;
     private Long totalPouchesChecked;
     private Long totalMinutes;
-
-    /**
-     * Constructs a new UserProductivityQueryDTO with the given parameters.
-     *
-     * @param username            The username of the user
-     * @param totalSubmissions    Total number of submissions made by the user
-     * @param totalPouchesChecked Total number of pouches checked by the user
-     * @param totalMinutes        Total time spent on tasks in minutes
-     */
-    public UserProductivityQueryDTO(String username, Long totalSubmissions, Long totalPouchesChecked, Long totalMinutes) {
-        this.username = username;
-        this.totalSubmissions = totalSubmissions;
-        this.totalPouchesChecked = totalPouchesChecked;
-        this.totalMinutes = totalMinutes;
-    }
-
-    // Getters
-    /** @return The username of the user */
-    public String getUsername() { return username; }
-
-    /** @return The total number of submissions made by the user */
-    public Long getTotalSubmissions() { return totalSubmissions; }
-
-    /** @return The total number of pouches checked by the user */
-    public Long getTotalPouchesChecked() { return totalPouchesChecked; }
-
-    /** @return The total time spent on tasks in minutes */
-    public Long getTotalMinutes() { return totalMinutes; }
-
-    // TODO: Consider adding a method to calculate productivity rate (e.g., pouches per hour)
-    // TODO: Implement data validation to ensure all fields contain valid values
 }
